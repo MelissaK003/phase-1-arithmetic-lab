123c359a5a2e063a5e7fd39bbba9110801fb71f0
Melissa <melissakiptoo@gmail.com>
2024-09-25 12:40:35 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 31; 
+const num2 = 2;  
+const multiply = num1 * num2; 
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 44;
+const num4 = 8;
+const mod = num3 % num4 
+
+const max = Math.max (7,10,20,14);
